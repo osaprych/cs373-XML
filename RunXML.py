832f0a7653d666b07fed70d7ec4cd885d7acf35c
Shaelyn-Watson <shaelynjoy@gmail.com>
2013-06-17 11:41:16 -0500

    Created skeleton RunXML to kickoff dev

---
+++ RunXML.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+To run the programXML.out
+    % chmod ugo+x RunXML.py
+    % RunXML.py < RunXML.in > RunXML.out
+
+To document the program
+    % pydoc -w XML
+"""
+
+# -------
+# imports
+# -------
+
+import sys
+
+#from XML import xml_solve
+
+# ----
+# main
+# ----
+
+#xml_solve(sys.stdin, sys.stdout)
